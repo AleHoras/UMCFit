0fb7cc507055a94f7ab7c7145f82bac67f1f3d76
Diego-k2 <dSilva@netsoft.com>
2023-05-19 17:38:01 -0300

    feat: Ligação da telas e organização do código

--- app/src/main/java/com/example/umcfit_calculadoraimc/ImcActivity.kt
+++ app/src/main/java/com/example/umcfit_calculadoraimc/ImcActivity.kt
@@ -1,7 +1,13 @@
 package com.example.umcfit_calculadoraimc
 
+import android.content.Context
+import android.content.Intent
+import android.inputmethodservice.InputMethodService
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
 import android.widget.TextView
 import java.math.RoundingMode
 import java.text.DecimalFormat
@@ -12,43 +18,85 @@ class ImcActivity : AppCompatActivity() {
         setContentView(R.layout.activity_calc_imc)
         try{
 
-            findViewById<TextView>(R.id.btnCalcular).setOnClickListener {
-
-                var txtAltura = findViewById<TextView>(R.id.txtAltura).text.toString();
-                var txtPeso = findViewById<TextView>(R.id.txtPeso).text.toString();
-
-                var altura = txtAltura.toDouble();
-                var peso =  txtPeso.toDouble();
-
-                var resultadoConta = peso / ((altura / 100) * (altura / 100));
-
-                var decimalFormat = DecimalFormat("#.##");
-                decimalFormat.roundingMode = RoundingMode.UP;
-                val finalResult = decimalFormat.format(resultadoConta).toString();
-
-                var resultado = findViewById<TextView>(R.id.txtResultado);
-                resultado.text = finalResult
-
-
-                if(resultadoConta < 18.5 ){
-                    resultado.setTextColor(getResources().getColor(R.color.magreza));
-                } else if(resultadoConta >= 18.5 && resultadoConta <= 24.9){
-                    resultado.setTextColor(getResources().getColor(R.color.normal));
-                } else if(resultadoConta > 24.9 && resultadoConta <= 29.9){
-                    resultado.setTextColor(getResources().getColor(R.color.excesso));
-                } else if(resultadoConta > 29.9 && resultadoConta <= 34.9){
-                    resultado.setTextColor(getResources().getColor(R.color.obesidade_1));
-                } else if(resultadoConta > 34.9 && resultadoConta <= 39.9){
-                    resultado.setTextColor(getResources().getColor(R.color.obesidade_2));
-                } else if(resultadoConta > 39.9){
-                    resultado.setTextColor(getResources().getColor(R.color.obesidade_3));
+            findViewById<TextView>(R.id.txtAltura).addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged( s: CharSequence?,start: Int,count: Int, after: Int ) {
                 }
 
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                    TODO("Not yet implemented")
+                }
+
+                override fun afterTextChanged(s: Editable?) {
+                    if(s?.length == 3){
+                        closeKeyBoard();
+                    }
+                }
+
+            })
+
+
+
+            findViewById<TextView>(R.id.btnCalcular).setOnClickListener {
+
+                var txtAltura = findViewById<TextView>(R.id.txtAltura).text.toString().replace("/\\D/g", "");
+                var txtPeso = findViewById<TextView>(R.id.txtPeso).text.toString().replace("/\\D/g", "");
+                if(txtAltura.isNotEmpty() && txtPeso.isNotEmpty()){
+                    calculateImc(txtAltura, txtPeso);
+                }
             }
+
+            findViewById<TextView>(R.id.btnTabela).setOnClickListener {
+                goTabelaImc();
+            }
+
 
         } catch (error:Exception){
 
         }
     }
+
+    private fun calculateImc(txtAltura:String, txtPeso:String){
+        var altura = txtAltura.toDouble();
+        var peso =  txtPeso.toDouble();
+
+        var resultadoConta = peso / ((altura / 100) * (altura / 100));
+
+        var decimalFormat = DecimalFormat("#.##");
+        decimalFormat.roundingMode = RoundingMode.UP;
+        val finalResult = decimalFormat.format(resultadoConta).toString();
+
+        var resultado = findViewById<TextView>(R.id.txtResultado);
+        resultado.text = finalResult
+
+
+        if(resultadoConta < 18.5 ){
+            resultado.setTextColor(getResources().getColor(R.color.magreza));
+        } else if(resultadoConta >= 18.5 && resultadoConta <= 24.9){
+            resultado.setTextColor(getResources().getColor(R.color.normal));
+        } else if(resultadoConta > 24.9 && resultadoConta <= 29.9){
+            resultado.setTextColor(getResources().getColor(R.color.excesso));
+        } else if(resultadoConta > 29.9 && resultadoConta <= 34.9){
+            resultado.setTextColor(getResources().getColor(R.color.obesidade_1));
+        } else if(resultadoConta > 34.9 && resultadoConta <= 39.9){
+            resultado.setTextColor(getResources().getColor(R.color.obesidade_2));
+        } else if(resultadoConta > 39.9){
+            resultado.setTextColor(getResources().getColor(R.color.obesidade_3));
+        }
+    }
+
+    private fun goTabelaImc(){
+        val goTabelaImc = Intent(this, TabelaImcActivity::class.java);
+        startActivity(goTabelaImc);
+        finish();
+    }
+
+
+    private fun closeKeyBoard(){
+        val view: View? = currentFocus;
+        view?.let {
+//            val imm: InputMethodService = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodService;
+        }
+    }
+
 
 }
--- app/src/main/java/com/example/umcfit_calculadoraimc/TabelaImcActivity.kt
+++ app/src/main/java/com/example/umcfit_calculadoraimc/TabelaImcActivity.kt
@@ -1,11 +1,24 @@
 package com.example.umcfit_calculadoraimc
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class TabelaImcActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tb_imc)
+
+        findViewById<TextView>(R.id.btnVoltar).setOnClickListener{
+            goToCalculadora();
+        }
     }
+
+    private fun goToCalculadora(){
+        val goToCalculadora = Intent(this, ImcActivity::class.java);
+        startActivity(goToCalculadora);
+        finish();
+    }
+
 }
