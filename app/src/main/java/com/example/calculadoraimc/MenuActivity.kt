0d1d70f73a05d2e01f28dc31e04ed8a3a581dd1a
Diego-k2 <dSilva@netsoft.com>
2023-05-30 19:19:38 -0300

    Finaliza Front

--- app/src/main/java/com/example/calculadoraimc/MenuActivity.kt
+++ app/src/main/java/com/example/calculadoraimc/MenuActivity.kt
@@ -4,10 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
 
-class MainActivity : AppCompatActivity() {
+class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_menu)
 
         findViewById<TextView>(R.id.btnDadosPessoais).setOnClickListener {
 //            val goToCalculadora = Intent(this, );
